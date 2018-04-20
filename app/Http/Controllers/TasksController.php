5f6eb909ae28ee6f0dec1eba8865827503066ef5
chihiro.shimoyoma <chihiro.shimoyoma>
2018-04-20 13:14:33 +0000

    tasklist bootstrap

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,11 +45,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'status' => 'required|max10',
-            'content' => 'required|max191',
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
         ]);
         
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -69,10 +70,11 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'status' => 'required|max:10',
-            'content' => 'required|max191',
+            'content' => 'required|max:191',
         ]);
         
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,15 +4,36 @@
 
     <h1>タスク新規作成ページ</h1>
     
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::label('status', 'スターテス：') !!}
-        {!! Form::text('status') !!}
-    
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+
+             {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        <div class="row">
+            <div class="form-group">
+                <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-offset-6">
+                    {!! Form::label('status', 'スターテス：') !!}
+                </div>
+                <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-offset-6">
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+            </div>
+        </div>
         
-        {!! Form::submit('投稿') !!}
+        
+         <div class="row">
+            <div class="form-group">
+                <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-offset-6">
+                    {!! Form::label('content', 'タスク:') !!}
+                </div>
+                <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-offset-6">
+                    {!! Form::text('content', null, ['class' =>'form-control']) !!}
+                </div>
+            </div>
+        </div>
+    
+   
+    
+
+        
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
     {!! Form::close() !!}
 
